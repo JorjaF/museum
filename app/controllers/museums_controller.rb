870b5cd123699af2e0f04d4aceb0cfd2ef72f669
Jorja Fleming <aset284@gmail.com>
2023-07-07 15:26:00 -0600

    Feat: added parent new and parent index

---
+++ app/controllers/museums_controller.rb
@@ -0,0 +1,24 @@
+class MuseumsController < ApplicationController
+  def index
+    @museums = Museum.all
+  end
+
+  def new
+  end
+
+  def create
+    museum = Museum.new({
+      name: params[:museum][:name],
+      number_of_galleries: params[:museum][:number_of_galleries].to_i,
+      open_to_public: !!params[:museum][:open_to_public]
+    })
+
+    museum.save
+  end
+
+  def show
+    @museum = Museum.find(params[:id])
+  end
+
+
+end
