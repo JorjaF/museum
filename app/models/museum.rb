384496af55a0a4c8aeef43013e85fd9937fa0efe
Jorja Fleming <aset284@gmail.com>
2023-07-07 18:00:44 -0600

    Feat: Parent Children Index

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -1,4 +1,3 @@
 class Museum < ApplicationRecord
-
+    has_many :galleries
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,11 @@ Rails.application.routes.draw do
   get "/museums/new", to: "museums#new"
   post "/museums", to: "museums#create"
   get "/museum/:id", to: "museums#show"
+  get "/museum/:id/galleries", to: "museums#show_galleries"
 
   get "/galleries/new", to: "galleries#new"
   get "/galleries", to: "galleries#index"
   post "/galleries", to: "galleries#create"
   get "/gallery/:id", to: "galleries#show"
+
 end
