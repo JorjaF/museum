1f02e3244585647b31476cf4040472b632205336
Jorja Fleming <aset284@gmail.com>
2023-07-13 09:59:52 -0600

    added stories

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -27,11 +27,6 @@ class MuseumsController < ApplicationController
     @galleries = @museum.galleries
   end
 
-#   As a visitor
-# When I visit the parent index,
-# I see that records are ordered by most recently created first
-# And next to each of the records I see when it was created
-
   def most_recently_created
     @museums = Museum.all
     @museums = @museums.order(:created_at => :desc)
